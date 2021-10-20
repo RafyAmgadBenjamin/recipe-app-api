7b14ec9b422704205819fca20ae7ab1e713e4625
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2021-10-20 17:31:00 +0200

    add filtering for tags and ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -16,7 +16,11 @@ class BaseRecipeAttrViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixi
     ## We are overriding getting all the tags/ingredients in queryset (overriding the queryset)
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by("-name")
+        assigned_only = bool(int(self.request.query_params.get("assigned_only", 0)))
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+        return queryset.filter(user=self.request.user).order_by("-name").distinct()
 
     # overriding the create option to assign the tag/ingredients to the correct user
     def perform_create(self, serializer):
